410cb701221c47bf89e7354fa2fc08e31df378eb
cassiboy1203 <casvstb@gmail.com>
2024-09-06 23:23:17 +0200

    Initial commit

---
+++ src/main/java/io/github/cassiboy1203/staffmanagerCore/StaffmanagerCore.java
@@ -0,0 +1,17 @@
+package io.github.cassiboy1203.staffmanagerCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffmanagerCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
